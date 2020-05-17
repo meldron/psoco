2d49a34d0226dac135dd7403e7349686bed193db
Bernd Kaiser <bk@dfjk.eu>
2020-05-18 00:26:42 +0200

    build_docker.sh:
    fix image name typo

--- build_docker.sh
+++ build_docker.sh
@@ -4,6 +4,7 @@ set -e
 
 CACHE_DIR=docker_cache
 BUILD_DIR=build
+IMAGE_NAME=psoco-builder
 
 cd "$(dirname "$0")"
 
@@ -13,13 +14,13 @@ mkdir -p "${CACHE_DIR}/target"
 
 mkdir -p "${BUILD_DIR}"
 
-sudo docker build . -t psco-builder
+sudo docker build . -t "$IMAGE_NAME"
 
 sudo docker run --rm -it \
     -v "$(pwd)/${CACHE_DIR}/git:/home/rust/.cargo/git" \
     -v "$(pwd)/${CACHE_DIR}/cargo-registry":/home/rust/.cargo/registry \
     -v "$(pwd)/${CACHE_DIR}/target:/home/rust/src/target" \
-    psco-builder cargo build --release
+    "$IMAGE_NAME" cargo build --release
 
 cp "${CACHE_DIR}/target/x86_64-unknown-linux-musl/release/psoco" "${BUILD_DIR}"
 strip "${BUILD_DIR}/psoco"
